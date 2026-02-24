8ce5e4545e6d1ecd2aa59acd3361063072d4f842
Raul Marchan Catala <alountk@gmail.com>
2026-02-24 12:21:20 +0100

    feat: Integrate TypeORM for PostgreSQL database connection using environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,35 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './infrastructure/auth.module';
 import { HealthController } from './infrastructure/controllers/health.controller';
 import { JobModule } from './infrastructure/job.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // CAUTION!!! Only in dev mode.
+        ssl:
+          configService.get<string>('DB_SSL') === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : false,
+      }),
     }),
     AuthModule,
     JobModule,
