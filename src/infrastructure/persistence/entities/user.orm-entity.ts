ac189bb4783df6d7af687d070732f82d3ac8e328
Raul Marchan Catala <alountk@gmail.com>
2026-02-25 09:56:12 +0100

    feat: Add TypeORM entities for User and JobApplication, and a mapper for User.

---
+++ src/infrastructure/persistence/entities/user.orm-entity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class UserOrmEntity {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
